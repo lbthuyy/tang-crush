bd426656ccf6081e66f656bbee7f0dc2c6e71a1d
lbthuyy <118186556+lbthuyy@users.noreply.github.com>
2022-11-14 21:21:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Puzzlies Gen 5 à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnYes: 'Vẫn cứ là yêu CNN Puzzles <33',
+    btnNo: 'Không, ghét CLB í :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại thuộc về CNN Puzzles ❤',
+    btnReply: 'Gửi cho anh chị ❤',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'Anh chị biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Hẹn gặp Puzzlies của anh chị vào một ngày không xa ><.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    
 }
